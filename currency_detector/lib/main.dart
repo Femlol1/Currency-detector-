05f4aeb9741de22c886387e538cb916ee4fb0ea3
Femi osibemekun <oo158@student.le.ac.uk>
2023-04-23 23:47:19 +0100

    changes

--- currency_detector/lib/main.dart
+++ currency_detector/lib/main.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:tflite/tflite.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+import 'package:image/image.dart' as img;
+import 'dart:typed_data';
 
 void main() {
   runApp(CurrencyDetectorApp());
@@ -27,7 +29,7 @@ class CurrencyDetectorHomePage extends StatefulWidget {
 
 class _CurrencyDetectorHomePageState extends State<CurrencyDetectorHomePage> {
   final ImagePicker _picker = ImagePicker();
-  File _image;
+  File? _image; // Makes File type nullable
   String _detectedCurrency = '';
 
   @override
@@ -37,14 +39,13 @@ class _CurrencyDetectorHomePageState extends State<CurrencyDetectorHomePage> {
   }
 
   @override
-  void dispose() {
-    Tflite.close();
-    super.dispose();
+  void close() {
+    interpreter.dispose();
   }
 
   Future<void> loadModel() async {
     try {
-      await Tflite.loadModel(
+      await interpreter.loadModel(
         model: "assets/model.tflite",
         labels: "assets/labels.txt",
       );
@@ -62,28 +63,54 @@ class _CurrencyDetectorHomePageState extends State<CurrencyDetectorHomePage> {
       setState(() {
         _image = File(pickedFile.path);
       });
-      detectCurrency(_image);
+      detectCurrency(_image!);
     } else {
       print('No image selected.');
     }
   }
 
-  Future<void> detectCurrency(File image) async {
-    if (image == null) return;
 
-    final result = await Tflite.runModelOnImage(
-      path: image.path,
-      // Set the appropriate parameters based on your model
-    );
 
-    if (result != null && result.isNotEmpty) {
-      setState(() {
-        _detectedCurrency = result[0]['label'];
-      });
-    } else {
-      print('No currency detected.');
+Future<void> detectCurrency(File image) async {
+  if (image == null) return;
+
+  final interpreter = await Interpreter.fromAsset("assets/model.tflite");
+
+  // Resize the image to the expected input size (e.g., 224x224)
+  img.Image originalImage = img.decodeImage(image.readAsBytesSync());
+  img.Image resizedImage = img.copyResize(originalImage, width: 224, height: 224);
+
+  // Convert the image data to a Float32List
+  var inputData = imageToFloat32List(resizedImage);
+
+  // Allocate memory for the output tensor
+  var outputData = List<List<double>>.filled(1, List<double>.filled(5, 0));
+
+
+  // Run the interpreter
+  await interpreter.runForMultipleInputs([inputData], outputData);
+
+  // Process the output data and update the _detectedCurrency variable
+  // ...
+
+  interpreter.dispose();
+}
+
+Float32List imageToFloat32List(img.Image image) {
+  var convertedBytes = Float32List(1 * 224 * 224 * 3);
+  var buffer = Float32List.view(convertedBytes.buffer);
+  int pixelIndex = 0;
+  for (int i = 0; i < 224; i++) {
+    for (int j = 0; j < 224; j++) {
+      var pixel = image.getPixel(j, i);
+      buffer[pixelIndex++] = (img.getRed(pixel)) / 255.0;
+      buffer[pixelIndex++] = (img.getGreen(pixel)) / 255.0;
+      buffer[pixelIndex++] = (img.getBlue(pixel)) / 255.0;
     }
   }
+  return convertedBytes;
+}
+
 
   @override
   Widget build(BuildContext context) {
