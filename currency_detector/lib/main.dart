83f4db569d2f159b8547db72e6ad958506e90ef8
Femi osibemekun <oo158@student.le.ac.uk>
2023-04-25 19:49:25 +0100

    initialised firebase with my app

--- currency_detector/lib/main.dart
+++ currency_detector/lib/main.dart
@@ -1,60 +1,13 @@
-/*import 'dart:io';
+import 'dart:io';
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import 'package:image/image.dart' as img;
-import 'dart:typed_data';*/
-
-
-
-import 'package:english_words/english_words.dart';
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'dart:typed_data';
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => MyAppState(),
-      child: MaterialApp(
-        title: 'Namer App',
-        theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
-        ),
-        home: MyHomePage(),
-      ),
-    );
-  }
-}
-
-class MyAppState extends ChangeNotifier {
-  var current = WordPair.random();
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
-    return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-        ],
-      ),
-    );
-  }
-}
-
-/*void main() {
   runApp(CurrencyDetectorApp());
 }
 
@@ -73,21 +26,25 @@ class CurrencyDetectorApp extends StatelessWidget {
 
 class CurrencyDetectorHomePage extends StatefulWidget {
   @override
-  _CurrencyDetectorHomePageState createState() => _CurrencyDetectorHomePageState();
+  _CurrencyDetectorHomePageState createState() =>
+      _CurrencyDetectorHomePageState();
 }
 
 class _CurrencyDetectorHomePageState extends State<CurrencyDetectorHomePage> {
   final ImagePicker _picker = ImagePicker();
   File? _image; // Makes File type nullable
-  String _detectedCurrency = '';
+  String _detectedCurrency = ' ';
+  List<String>? _labels;
+
+  get interpreter => null;
 
   @override
   void initState() {
     super.initState();
     loadModel();
+    loadLabels();
   }
 
-  @override
   void close() {
     interpreter.dispose();
   }
@@ -100,6 +57,15 @@ class _CurrencyDetectorHomePageState extends State<CurrencyDetectorHomePage> {
       );
     } catch (e) {
       print('Error loading model: $e');
+    }
+  }
+
+  Future<void> loadLabels() async {
+    try {
+      String labelsData = await rootBundle.loadString('assets/labels.txt');
+      _labels = labelsData.split('\n');
+    } catch (e) {
+      print('Error loading labels: $e');
     }
   }
 
@@ -118,48 +84,76 @@ class _CurrencyDetectorHomePageState extends State<CurrencyDetectorHomePage> {
     }
   }
 
+  Future<void> detectCurrency(File image) async {
+    if (image == null) return;
 
+    final interpreter = await Interpreter.fromAsset("assets/model.tflite");
 
-Future<void> detectCurrency(File image) async {
-  if (image == null) return;
+    // Resize the image to the expected input size (e.g., 224x224)
+    img.Image? originalImage = img.decodeImage(image.readAsBytesSync());
+    img.Image resizedImage =
+        img.copyResize(originalImage!, width: 224, height: 224);
 
-  final interpreter = await Interpreter.fromAsset("assets/model.tflite");
+    // Convert the image data to a Float32List
+    var inputData = imageToFloat32List(resizedImage);
 
-  // Resize the image to the expected input size (e.g., 224x224)
-  img.Image originalImage = img.decodeImage(image.readAsBytesSync());
-  img.Image resizedImage = img.copyResize(originalImage, width: 224, height: 224);
+    // Allocate memory for the output tensor
+    var outputData = List<List<double>>.filled(1, List<double>.filled(5, 0));
 
-  // Convert the image data to a Float32List
-  var inputData = imageToFloat32List(resizedImage);
+    // Run the interpreter
+    interpreter
+        .runForMultipleInputs([inputData], outputData as Map<int, Object>);
 
-  // Allocate memory for the output tensor
-  var outputData = List<List<double>>.filled(1, List<double>.filled(5, 0));
+    // Process the output data and update the _detectedCurrency variable
+    int detectedIndex = outputData[0].indexOf(outputData[0].reduce(max));
+    String detectedLabel = _labels![detectedIndex];
+    setState(() {
+      _detectedCurrency = detectedLabel;
+    });
 
-
-  // Run the interpreter
-  await interpreter.runForMultipleInputs([inputData], outputData);
-
-  // Process the output data and update the _detectedCurrency variable
-  // ...
-
-  interpreter.dispose();
-}
-
-Float32List imageToFloat32List(img.Image image) {
-  var convertedBytes = Float32List(1 * 224 * 224 * 3);
-  var buffer = Float32List.view(convertedBytes.buffer);
-  int pixelIndex = 0;
-  for (int i = 0; i < 224; i++) {
-    for (int j = 0; j < 224; j++) {
-      var pixel = image.getPixel(j, i);
-      buffer[pixelIndex++] = (img.getRed(pixel)) / 255.0;
-      buffer[pixelIndex++] = (img.getGreen(pixel)) / 255.0;
-      buffer[pixelIndex++] = (img.getBlue(pixel)) / 255.0;
-    }
+    interpreter.close();
   }
-  return convertedBytes;
-}
 
+  /*Future<void> detectCurrency(File image) async {
+    if (image == null) return;
+
+    final interpreter = await Interpreter.fromAsset("assets/model.tflite");
+
+    // Resize the image to the expected input size (e.g., 224x224)
+    img.Image? originalImage = img.decodeImage(image.readAsBytesSync());
+    img.Image resizedImage =
+        img.copyResize(originalImage!, width: 224, height: 224);
+
+    // Convert the image data to a Float32List
+    var inputData = imageToFloat32List(resizedImage);
+
+    // Allocate memory for the output tensor
+    var outputData = List<List<double>>.filled(1, List<double>.filled(5, 0));
+
+    // Run the interpreter
+    interpreter
+        .runForMultipleInputs([inputData], outputData as Map<int, Object>);
+
+    // Process the output data and update the _detectedCurrency variable
+    // ...
+
+    interpreter.close();
+  }*/
+
+  Float32List imageToFloat32List(img.Image image) {
+    var convertedBytes = Float32List(1 * 224 * 224 * 3);
+    var buffer = Float32List.view(convertedBytes.buffer);
+    int pixelIndex = 0;
+    for (int i = 0; i < 224; i++) {
+      for (int j = 0; j < 224; j++) {
+        var pixel = image.getPixel(j, i);
+        buffer[pixelIndex++] = pixel.r / 255.0; // Red channel
+        buffer[pixelIndex++] = pixel.g / 255.0; // Green channel
+        buffer[pixelIndex++] = pixel.b / 255.0; // Blue channel
+      }
+    }
+    return convertedBytes;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -205,4 +199,3 @@ Float32List imageToFloat32List(img.Image image) {
     );
   }
 }
-*/
