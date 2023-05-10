8c5eece783fd9cd9047aa0ce854f18cc630cec64
Femi osibemekun <oo158@student.le.ac.uk>
2023-05-10 12:32:30 +0100

    added comments to the autentication page

--- currency_detector/lib/auth.dart
+++ currency_detector/lib/auth.dart
@@ -1,37 +1,45 @@
+//impports the nessasry libraries
 import 'package:currency_detector/main.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-import 'home.dart';
 import 'login.dart';
 
+//authentication class
 class Auth {
   handleAuthState() {
     return StreamBuilder(
         stream: FirebaseAuth.instance.authStateChanges(),
         builder: (BuildContext context, snapshot) {
+          // If the snapshot contains data, that means the user is authenticated
           if (snapshot.hasData) {
-            return SwipeNavigation() /*HomePage()*/;
+            // So, we send them to the home page
+            return SwipeNavigation(); //or main page ;
           } else {
+            // If the snapshot doesn't contain data, that means the user is not authenticated
+            // So, we show them the LoginPage
             return const LoginPage();
           }
         });
   }
 
+//google sign in function
   signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser =
           await GoogleSignIn(scopes: <String>["email"]).signIn();
-
+// If the Google sign-in was successful
       if (googleUser != null) {
         final GoogleSignInAuthentication googleAuth =
             await googleUser.authentication;
+        // Create a new authentication credential using the Google sign-in details
         final credential = GoogleAuthProvider.credential(
           accessToken: googleAuth.accessToken,
           idToken: googleAuth.idToken,
         );
+        // Sign into Firebase with the Google credential
         return await FirebaseAuth.instance.signInWithCredential(credential);
       } else {
         // Handle the case when the user cancels the sign-in process
@@ -44,7 +52,9 @@ class Auth {
     }
   }
 
+//sign out function
   signOut() {
+    //calls the Firebase signout function
     FirebaseAuth.instance.signOut();
   }
 }
--- currency_detector/lib/cd.dart
+++ currency_detector/lib/cd.dart
@@ -3,7 +3,7 @@ import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:tflite/tflite.dart';
-import 'package:tflite_flutter/tflite_flutter.dart';
+
 import 'package:image/image.dart' as img;
 import 'package:flutter/services.dart';
 
